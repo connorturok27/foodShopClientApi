cfde4c33d46fd65951e7cf61955f1e02ec91dbca
Christian Martinez <Christian.Martinez@udr.com>
2020-10-29 13:27:32 -0600

    (clientApi/34) - Get single product endpoint

---
+++ constants/errorMessages.js
@@ -0,0 +1,6 @@
+exports.CODE1 = (item) =>  {
+    return {
+        code: 1,
+        message: `${item} not found`
+    }
+}
--- routes/public.routes.js
+++ routes/public.routes.js
@@ -5,6 +5,7 @@ const productController = require('../controllers/product/product.controller');
 const foodTypeController = require('../controllers/product/foodType.controller');
 
 router.get('/food', productController.getProducts);
+router.get('/food/:foodId', productController.getSingleProduct);
 router.get('/foodType', foodTypeController.getFoodTypes);
 
 module.exports = router;
